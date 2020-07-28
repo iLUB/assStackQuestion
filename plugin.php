32be98dc2aebf68faf8ff14ce1685b81f469c919
Timon Amstutz <timon.amstutz@bluewin.ch>
2020-07-28 17:23:31 +0200

    Update Plugin Version

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@ $version = "3.2.2";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "6.0.0";
-$ilias_max_version = "6.0.999";
+$ilias_max_version = "6.999.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann, Jesus Copado";
